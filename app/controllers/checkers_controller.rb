457ea82b9d677abee0a271b1d4847c85506617b5
Daniel <dpericich@gmail.com>
2022-11-02 19:06:06 -0500

    created checkers controller

---
+++ app/controllers/checkers_controller.rb
@@ -0,0 +1,13 @@
+class CheckersController < ApplicationController
+  def check_user(user_id)
+
+  end
+
+  # Need to figure out what I'm passing through here
+  def check_user_batch()
+  end
+
+
+  private
+  # sub methods here
+end
