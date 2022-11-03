550c21cd1c8a66981ec7953ea11ba603f8bbd2fa
Daniel <dpericich@gmail.com>
2022-11-02 22:48:05 -0500

    added individual user and user collection methods to checker_controller

--- app/controllers/checkers_controller.rb
+++ app/controllers/checkers_controller.rb
@@ -1,13 +1,38 @@
 class CheckersController < ApplicationController
-  def check_user(user_id)
+  # Some before_action to protect API from rogue actors
 
+  def check_user(user_id)
+    user = User.find(user_id)
+    service = user.service
+    dictionary = user.dictionary
+    current_attempts = 0;
+
+    while (current_attempts < service.attempts_before_backoff) do
+      response = service.attempt_login(dictionary.terms[user.current_dictionary_index])
+
+      if (response == "success")
+        user.update!(password: dictionary.terms[user.current_dictionary_index])
+        create_log(user, service, status)
+        render json: status
+      elsif (response == "failure")
+        new_dictionary_index += 1
+        user.update!(current_dictionary_index: new_dictionary_index)
+        current_attempts +=  1
+      elsif (response == "locked")
+        user.update(status: "locked")
+        create_log(user, service, status)
+        render json: status
+      end
+    end
   end
 
-  # Need to figure out what I'm passing through here
-  def check_user_batch()
+  def check_user_batch(user_collection)
+    # perform the user check on some service or other group of records
   end
 
 
   private
-  # sub methods here
+  def create_log(user, service, status)
+    EventLog.create!(user: user, service: service, status: status)
+  end
 end
