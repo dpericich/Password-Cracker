9fe2540178ae743228bc0d9ee75f419fa9c6c372
Daniel <dpericich@gmail.com>
2022-11-02 19:05:49 -0500

    updated schema with new and updated tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_18_215950) do
-  create_table "services", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.integer "try_before_lockout_count"
-    t.integer "attempt_backoff_minutes"
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_234713) do
+  create_table "dictionaries", force: :cascade do |t|
+    t.string "terms", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_research", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "service", null: false
-    t.integer "word_index", default: 0
-    t.datetime "last_login"
-    t.string "successful_password"
+  create_table "event_logs", force: :cascade do |t|
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "service_id"
+    t.index ["service_id"], name: "index_event_logs_on_service_id"
+    t.index ["user_id"], name: "index_event_logs_on_user_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "service_name"
+    t.string "base_url"
+    t.integer "attempts_before_account_freeze"
+    t.integer "attempts_before_backoff"
+    t.integer "duration_of_backoff"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "status"
+    t.integer "current_dictionary_index", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "service_id"
+    t.integer "dictionary_id"
+    t.index ["dictionary_id"], name: "index_users_on_dictionary_id"
+    t.index ["service_id"], name: "index_users_on_service_id"
   end
 
 end
