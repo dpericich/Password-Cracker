92bf595255ab53c8c363d0075355a7e8b1eb51c9
Daniel <dpericich@gmail.com>
2022-11-02 22:47:36 -0500

    added blocked_ip_origins array to model and method to call web automation tools

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,9 @@
 class Service < ApplicationRecord
+  serialize :blocked_ip_origins, Array
   # Will fill this out more as I work through the OWASP V2 docs
+
+  def attempt_login(current_term)
+    # Will call web automation tool and attempt to log in given a term
+    # based on the automation this will return "success", "fail", "locked"
+  end
 end
